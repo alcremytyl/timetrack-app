82a8d5c8462d94bb3f133f0984f565407a0b933e
Mytyl <39316039+alcremytyl@users.noreply.github>
2025-11-13 08:31:35 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = inputs: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import inputs.nixpkgs {inherit system;};
+    py = pkgs.python311.withPackages(p: with p;[
+        django
+        flask
+        sqlite-utils
+        requests
+        pyyaml
+    ]
+  );
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          py 
+          pkgs.sqlite
+          pkgs.git
+        ];
+        shellHook = ''
+          export FLAKE_DIR=$(pwd)
+        '';
+      };
+  };
+}
